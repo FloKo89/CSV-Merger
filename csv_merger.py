f31d232efe533cee5c40ab84cb85112fb442a9a8
Florian Kotulla <f.kotulla@gmx.de>
2023-04-30 22:31:13 +0200

    Update csv_merger.py

--- csv_merger.py
+++ csv_merger.py
@@ -5,7 +5,6 @@ import tkinter as tk
 from tkinter import filedialog
 from tkinter import ttk
 
-
 # Funktion zum Extrahieren der CSV-Zeilen anhand des Schlüsselworts
 def extract_csv_rows(csv_file, keyword):
     # Lesen der CSV-Datei in einen Pandas-Datenrahmen
@@ -24,21 +23,20 @@ def extract_csv_rows(csv_file, keyword):
     extracted_df = df[df.apply(lambda row: row.astype(str).str.contains(keyword).any(), axis=1)]
     return extracted_df
 
-
-
-
 # Funktion zum Durchsuchen des Eingabeverzeichnisses
 def browse_input_directory():
-    input_directory = filedialog.askdirectory()
-    input_directory_entry.delete(0, tk.END)
-    input_directory_entry.insert(0, input_directory)
+    input_directory = filedialog.askdirectory(title="Eingabeverzeichnis auswählen")
+    if input_directory:  # Stellen Sie sicher, dass der Benutzer ein Verzeichnis ausgewählt hat
+        input_directory_entry.delete(0, tk.END)
+        input_directory_entry.insert(0, input_directory)
 
 
 # Funktion zum Durchsuchen der Ausgabedatei
 def browse_output_file():
-    output_file = filedialog.asksaveasfilename(defaultextension=".csv")
-    output_file_entry.delete(0, tk.END)
-    output_file_entry.insert(0, output_file)
+    output_file = filedialog.asksaveasfilename(defaultextension=".csv", title="Ausgabedatei auswählen")
+    if output_file:  # Stellen Sie sicher, dass der Benutzer eine Datei ausgewählt hat
+        output_file_entry.delete(0, tk.END)
+        output_file_entry.insert(0, output_file)
 
 
 # Funktion zum Zusammenführen und Extrahieren der CSV-Dateien
@@ -71,62 +69,75 @@ def merge_csv_files():
     # Schreiben der bereinigten Daten in eine CSV-Datei
     combined_df.to_csv(output_file, index=False, header=False, sep=';', line_terminator='\n')
 
-
 # Funktion zum Ermitteln der Kodierung einer CSV-Datei
 def get_csv_encoding(file_path):
     with open(file_path, "rb") as f:
         result = chardet.detect(f.read())
         return result["encoding"]
+    
+def update_merge_button_state(*args):
+    if input_directory_entry.get() and output_file_entry.get() and keyword_var.get():
+        merge_button.config(state="normal")
+    else:
+        merge_button.config(state="disabled")
 
 
-# Erstellen des Hauptfensters und Festlegen des Titels
-root = tk.Tk()
-root.iconbitmap("icon.ico")
-root.title("CSV Merger")
 
-# Erstellen der Widgets für das Eingabeverzeichnis
-input_directory_label = tk.Label(root, text="Eingabeverzeichnis:")
-input_directory_label.grid(row=0, column=0, padx=5, pady=5, sticky=tk.W)
+root = tk.Tk() # Erstellen des Hauptfensters 
+root.iconbitmap("icon.ico") # Hinzufügen eines Programmsymbols
+root.title("CSV Merger") # Titel des Hauptfensters
+root.resizable(False, False)# Festlegen, dass die Größe des Hauptfensters nicht geändert werden kann
+root.columnconfigure(1, weight=1)  # Festlegen, dass die Spalte 1 des Hauptfensters beim Ändern der Fenstergröße gestreckt wird
+root.geometry("550x220")  # Festlegen der Fenstergröße
+root.eval('tk::PlaceWindow . center')  # Zentrieren des Hauptfensters
+root.configure(background="white")  # Festlegen der Hintergrundfarbe des Hauptfensters
 
-input_directory_entry = tk.Entry(root, width=50)
-input_directory_entry.grid(row=0, column=1, padx=5, pady=5)
+# Erstellen und platzieren der Widgets für das Eingabeverzeichnis
+input_directory_label = tk.Label(root, text="Eingabeverzeichnis:", font=("Helvetica", 12))
+input_directory_label.grid(row=0, column=0, padx=(15, 5), pady=(15, 5), sticky=tk.W)
+
+input_directory_entry = tk.Entry(root, width=50, font=("Helvetica", 12))
+input_directory_entry.grid(row=0, column=1, padx=5, pady=(15, 5))
 
 input_directory_button = tk.Button(
-    root, text="Durchsuchen", command=browse_input_directory
+    root, text="Durchsuchen", command=browse_input_directory, font=("Helvetica", 9)
 )
-input_directory_button.grid(row=0, column=2, padx=5, pady=5)
+input_directory_button.grid(row=0, column=2, padx=(5, 15), pady=(15, 5))
 
-# Erstellen der Widgets für das Ausgabeverzeichnis
-output_file_label = tk.Label(root, text="Ausgabedatei:")
-output_file_label.grid(row=1, column=0, padx=5, pady=5, sticky=tk.W)
+# Erstellen und platzieren der Widgets für das Ausgabeverzeichnis
+output_file_label = tk.Label(root, text="Ausgabedatei:", font=("Helvetica", 12))
+output_file_label.grid(row=1, column=0, padx=(15, 5), pady=5, sticky=tk.W)
 
-output_file_entry = tk.Entry(root, width=50)
+output_file_entry = tk.Entry(root, width=50, font=("Helvetica", 12))
 output_file_entry.grid(row=1, column=1, padx=5, pady=5)
 
-output_file_button = tk.Button(root, text="Durchsuchen", command=browse_output_file)
-output_file_button.grid(row=1, column=2, padx=5, pady=5)
+output_file_button = tk.Button(root, text="Durchsuchen", command=browse_output_file, font=("Helvetica", 9))
+output_file_button.grid(row=1, column=2, padx=(5, 15), pady=5)
 
-# Erstellen der Widgets für das Schlüsselwort
-keyword_label = tk.Label(root, text="Schlüsselwort:")
-keyword_label.grid(row=2, column=0, padx=5, pady=5, sticky=tk.W)
+# Erstellen und platzieren der Widgets für das Schlüsselwort
+keyword_label = tk.Label(root, text="Schlüsselwort:", font=("Helvetica", 12))
+keyword_label.grid(row=2, column=0, padx=(15, 5), pady=5, sticky=tk.W)
+keyword_var = tk.StringVar()
+keyword_var.trace("w", update_merge_button_state)
 
-keyword_entry = tk.Entry(root, width=50)
+keyword_entry = tk.Entry(root, width=50, textvariable=keyword_var, font=("Helvetica", 12))
 keyword_entry.grid(row=2, column=1, padx=5, pady=5)
 
-# Erstellen der Widgets für den Merge-Button
+# Erstellen und platzieren der Widgets für den Merge-Button
 merge_button = tk.Button(
-    root, text="CSV-Dateien zusammenführen und extrahieren", command=merge_csv_files
+    root, text="CSV-Dateien zusammenführen und extrahieren", command=merge_csv_files, state="disabled", font=("Helvetica", 9)
 )
 merge_button.grid(row=3, column=1, padx=5, pady=5)
 
-# Erstellen der Widgets für die Fortschrittsanzeige
+# Erstellen und platzieren der Widgets für die Fortschrittsanzeige
 progress_bar = ttk.Progressbar(
     root, orient="horizontal", length=300, mode="determinate"
 )
 progress_bar.grid(row=4, column=1, padx=5, pady=5)
 
-progress_label = tk.Label(root, text="Fortschritt: 0%")
+progress_label = tk.Label(root, text="Fortschritt: 0%", font=("Helvetica", 12))
 progress_label.grid(row=5, column=1, padx=5, pady=5)
 
 # Starten der Mainloop
 root.mainloop()
+
